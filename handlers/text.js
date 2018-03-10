38912ff7a537549cddb30e7a173e95e360242050
panafana <panafana@yahoo.gr>
2018-03-10 18:43:56 +0200

    test message11

--- handlers/text.js
+++ handlers/text.js
@@ -1,3 +1,5 @@
+import { resolve } from 'dns';
+
 const m = require('../messages/msgTemplates');
 
 function textHandler (message, id, nlp, user) {
@@ -7,10 +9,24 @@ function textHandler (message, id, nlp, user) {
         case "HODOR": return m.hodorDefault(id);
         case "TEST": return m.testDefault(id);
         case "HELLO BROTHER": return m.helloBrother(id);
-        default: throw new Error("Unrecognized text Message ");
+        default: return nlpHandler(id,nlp,user);
     }
 }
 
+function nlpHandler(id , nlp,user){
+    return new Promise((resolve,reject) =>{
+        if(nlp.entities.intent){
+            resolve(intentHandler(id,user,nlp));
+        }
+    } )
+}
+
+function intentHandler(id,user,nlp){
+    switch(nlp.entities.intent[0].value){
+        case "greeting":return m.getStarted(id,user);
+        default: throw new Error("Unknown intent");
+    }
+}
 module.exports = {
     textHandler
 };
