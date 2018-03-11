f8028486b73003fb8fb3382d8032e36767ad42d0
panafana <panafana@yahoo.gr>
2018-03-11 16:35:52 +0200

    event

---
+++ data.js
@@ -0,0 +1,106 @@
+module.exports = [
+    {
+        title: "Spring Congress 2018",
+        type: "Operational",
+        commitment: "LC Krakow",
+        country: "Poland",
+        applications: false,
+        image_url: "https://eestec.net/media/cache/96/85/9685e8982bad31d13dd06fca38ac1ea9.jpg"
+    },
+    {
+        title: "10th Training for Trainers",
+        type: "Operational",
+        commitment: "LC Sarajevo",
+        country: "Bosnia",
+        applications: false,
+        image_url: "https://eestec.net/media/cache/96/85/9685e8982bad31d13dd06fca38ac1ea9.jpg"
+    },
+    {
+        title: "BrAIns",
+        type: "Workshop",
+        commitment: "LC Madrid",
+        country: "Spain",
+        applications: true,
+        image_url: "https://eestec.net/media/cache/96/85/9685e8982bad31d13dd06fca38ac1ea9.jpg"
+    },
+    {
+        title: "deCode IT 2",
+        type: "Workshop",
+        commitment: "LC Bucharest",
+        country: "Romania",
+        applications: true,
+        image_url: "https://eestec.net/media/cache/19/c8/19c8eacba9b9e641b4fcbf852ade7fb9.jpg"
+    },
+    {
+        title: "First Latina Meeting",
+        type: "Exchange",
+        commitment: "LC Cosenza",
+        country: "Italia",
+        applications: true,
+        image_url: "https://eestec.net/media/cache/37/f5/37f51d18ee08721a9a186dfea5b0b872.jpg"
+    },
+    {
+        title: "IMW: the North",
+        type: "IMW",
+        commitment: "LC Riga",
+        country: "Latvia",
+        applications: true,
+        image_url: "https://eestec.net/media/cache/30/ba/30ba8c97e9e0f08f7698ed097d34e5b9.jpg"
+    },
+    {
+        title: "PrinterStellar",
+        type: "Workshop",
+        commitment: "LC Thessaloniki",
+        country: "Greece",
+        applications: true,
+        image_url: "https://eestec.net/media/cache/8f/31/8f3164377c6f7c3092eb836567c54df3.jpg"
+    },
+    {
+        title: "Regional Meeting: The Reach",
+        type: "Exhange",
+        commitment: "LC Izmir",
+        country: "Turkey",
+        applications: true,
+        image_url: "https://eestec.net/media/cache/1f/04/1f04f28d344d13270fc63d8b958a1ce6.jpg"
+    },
+    {
+        title: "TIEght Escape",
+        type: "Exhange",
+        commitment: "LC Delft",
+        country: "Netherlands",
+        applications: true,
+        image_url: "https://eestec.net/media/cache/14/bf/14bf17c81e9a3501c220900860cbae0e.jpg"
+    },
+    {
+        title: "Wappu Exchange",
+        type: "Exhange",
+        commitment: "LC Tampere",
+        country: "Finland",
+        applications: true,
+        image_url: "https://eestec.net/media/cache/da/f5/daf548cd27532367298fdd9b4400a581.jpg"
+    },
+    {
+        title: "Women in Engineering and Technology 4 LC Zurich",
+        type: "Workshop",
+        commitment: "LC Zurich",
+        country: "Switzerland",
+        applications: true,
+        image_url: "https://eestec.net/media/cache/2c/0c/2c0ccc62be6f5e36d86ab4367f2e21be.jpg"
+    },
+    {
+        title: "2nd Design Sprint",
+        type: "Sprint",
+        commitment: "LC Sarajevo",
+        country: "Bosnia",
+        applications: false,
+        image_url: "https://eestec.net/media/cache/67/40/674083791f552336cfb584b878a7abcf.jpg"
+    },
+    {
+        title: "Dr. Robot",
+        type: "Workshop",
+        commitment: "LC Athens",
+        country: "Greece",
+        applications: false,
+        image_url: "https://eestec.net/media/cache/f2/df/f2df37bb694d54ac454e54e7c32f4dc8.jpg"
+    }
+];
