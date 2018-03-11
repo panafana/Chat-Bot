b466ec61deacdbc706c4969d10f8ac34ad1a3710
panafana <panafana@yahoo.gr>
2018-03-11 18:42:34 +0200

    eventsa

--- messages/msgTemplates.js
+++ messages/msgTemplates.js
@@ -93,7 +93,7 @@ function eventSearch (id,nlp) {
 
   if(nlp.entities.type){
     let e = events.filter(e => (e.type==nlp.entities.type[0].value && e.applications));
-
+    console.log(e);
     setContext(id, {expecting: "nothing", step:"event_search",type:nlp.entities.type[0].value});
   return fb.fbMessageDelay(DELAY, id, {
     attachment: t.cardMessage(e.map(e => t.cardElement({
