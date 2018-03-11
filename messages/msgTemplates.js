3885d3a614bd09fac243e75025ef1b0b7b798364
panafana <panafana@yahoo.gr>
2018-03-11 18:49:26 +0200

    eventsa

--- messages/msgTemplates.js
+++ messages/msgTemplates.js
@@ -92,9 +92,9 @@ function unavailableEvents (id) {
 function eventSearch (id,nlp) {
 
   if(nlp.entities.type){
-    let e = events.filter(e => ((e.type==nlp.entities.type[0].value)));
-    console.log(e);
-    e = events.filter( e => !e.applications);
+    let f = events.filter( e => !e.applications);
+    console.log(f);
+    let e = f.filter(e => ((e.type==nlp.entities.type[0].value)));
     console.log(e);
     setContext(id, {expecting: "nothing", step:"event_search",type:nlp.entities.type[0].value});
   return fb.fbMessageDelay(DELAY, id, {
